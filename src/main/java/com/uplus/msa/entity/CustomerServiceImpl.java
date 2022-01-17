51d0897c5685da73d635315009d38f3845e2145d
mysoyul <vega2k_@naver.com>
2022-01-17 17:53:13 +0900

    service class add

--- src/main/java/com/uplus/msa/entity/CustomerServiceImpl.java
+++ src/main/java/com/uplus/msa/entity/CustomerServiceImpl.java
@@ -2,10 +2,27 @@ package com.uplus.msa.entity;
 
 import java.util.List;
 
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.uplus.msa.dto.CustomerDTO;
+import com.uplus.msa.repository.CustomerRepository;
 import com.uplus.msa.service.CustomerService;
 
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class CustomerServiceImpl implements CustomerService {
+	
+	private final CustomerRepository repository;
+	
+	//constructor injection
+//	public CustomerServiceImpl(CustomerRepository repository) {
+//		this.repository = repository;
+//	}
 
 	@Override
 	public List<CustomerDTO> getAllCustomers() throws Exception {
