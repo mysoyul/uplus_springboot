39bbfa5ea2eb0c94d1cf5425a2ce6efd2af8016b
mysoyul <vega2k_@naver.com>
2022-01-18 11:41:46 +0900

    controller update

--- src/main/java/com/uplus/msa/controller/CustomerController.java
+++ src/main/java/com/uplus/msa/controller/CustomerController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +33,9 @@ public class CustomerController {
 		return service.getAllCustomers();
 	}
 	
-	
+	@PostMapping
+	public Long createCustomer(@RequestBody CustomerDTO customerDTO) throws Exception {
+		return service.createCustomer(customerDTO);
+	}
 	
 }
