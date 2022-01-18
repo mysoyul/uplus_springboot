c8ae2b3f066bacd434193b492858e75944f3d2c1
mysoyul <vega2k_@naver.com>
2022-01-18 13:23:21 +0900

    controller update

--- src/main/java/com/uplus/msa/controller/CustomerController.java
+++ src/main/java/com/uplus/msa/controller/CustomerController.java
@@ -2,6 +2,7 @@ package com.uplus.msa.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,6 +28,10 @@ public class CustomerController {
 		return service.getCustomerById(id);
 	}
 	
+	@GetMapping("/re/{id}")
+	public ResponseEntity<?> getCustomerRE(@PathVariable Long id) throws Exception {
+		return service.getCustomerByIdRE(id);
+	}
 	
 	@GetMapping
 	public List<CustomerDTO> getAllCustomers() throws Exception {
